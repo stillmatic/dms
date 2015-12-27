ae1fe42bc38417257f4d66b140ee845305f1829e
Chris Hua <hua.christopher@gmail.com>
2015-12-27 01:04:06 -0800

    initial commit

---
+++ locations/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.conf import settings
+from datetime import datetime
+
+# Create your models here.
+class Checkin(models.Model):
+  check_user = models.ForeignKey(settings.AUTH_USER_MODEL)
+  check_time = models.DateTimeField('User checkin date/time',
+    default=datetime.now, blank=True)
